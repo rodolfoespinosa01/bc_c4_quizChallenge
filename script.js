7cc564d720055625e968ab8c03153a9396e212cb
Rodolfo <rodolfoespinosa01@gmail.com>
2023-09-12 15:58:17 -0400

    Inital Files to start with

---
+++ script.js
@@ -0,0 +1,20 @@
+
+//Determine the data
+
+// How do I store the data
+
+var questions = [
+  {
+    questionText: 'What does JS STand for',
+    choices: ['JustStand', 'JustSkydiving', 'JokeSyllabus', 'Javascript'],
+    correctAnswer: 'Javascript'
+  },
+  {
+    questionText: 'What is the color of the sky?',
+    choices: ['Green', 'Blue', 'Yellow', 'Black'],
+    correctIndex: 1
+  },
+  {}
+];
+
+console.log(questions[1].choices);
